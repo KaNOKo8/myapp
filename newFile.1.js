fc6bee47fb1df512a08d544f6467b96960f6d187
さめ <shark@samenoMacBook-Pro.local>
2024-06-24 21:15:58 +0900

    fix

--- newFile.1.js
+++
@@ -1,7 +0,0 @@
-var createError = require('http-errors');
-const { app } = require('./app');
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    next(createError(404));
-});
