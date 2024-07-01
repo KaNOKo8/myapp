296db5e3268b14f7d94b50065450273620566c93
さめ <shark@samenoMacBook-Pro.local>
2024-07-01 14:30:34 +0900

    add notes

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var helloRouter = require('./routes/hello');
+var notesRouter = require('./routes/notes'); 
 
 var app = express();
 exports.app = app;
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/hello', helloRouter);
+app.use('/notes', notesRouter);     
 // error handler
 app.use(function(req, res, next) {
     next(createError(404));
