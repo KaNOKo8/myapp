aa5857dd37d0a46dfe5c10769fee702221b54dbe
さめ <shark@samenoMacBook-Pro.local>
2024-07-01 22:33:10 +0900

    add notes

---
+++ insertMany.js
@@ -0,0 +1,25 @@
+const { MongoClient } = require("mongodb");
+const uri = "mongodb+srv://konoka24:abc@cluster0.u4922gb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const client = new MongoClient(uri);
+async function run() {
+const database = client.db('notes');
+const notes = database.collection('notes');
+// データを登録
+const query = [{
+id: 1,
+title: 'ノート１のタイトルです',
+subTitle: 'ノート１のサブタイトルです',
+bodyText: 'ノート１の本文です'
+},
+{
+id: 2,
+title: 'ノート２のタイトルです',
+subTitle: 'ノート２のサブタイトルです',
+bodyText: 'ノート２の本文です'
+}];
+const note = await notes.insertMany(query);
+console.log(note);
+// 最後にクロースする
+await client.close();
+}
+run()
